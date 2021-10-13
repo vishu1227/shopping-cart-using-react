1587da086a957b2e7a88e806e6276b8308659fb9
vishu1227 <bh27122000@gmail.com>
2021-10-13 17:59:37 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <>
+      <h1>Let get's started!</h1>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
+}
+
+export default App;
