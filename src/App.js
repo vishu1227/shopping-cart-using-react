14f888490330fd13651bcb9dd8ff6e041be0e1f4
vishu1227 <bh27122000@gmail.com>
2021-10-14 10:27:34 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <>
       <h1>Let get's started!</h1>
-      <ExpenseItem></ExpenseItem>
     </>
   );
 }
