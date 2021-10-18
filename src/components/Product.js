7ed82b42c83f35614598f90d84bdf51197c50897
vishu1227 <bh27122000@gmail.com>
2021-10-18 16:35:28 +0530

    modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Cart from "./cart";
+import './css/Product.css'
 
 export default function Product(props) {
   return (
     <>
       <img src={props.imageUrl} alt="" />
-      <p>${title}</p>
+      <p>{props.title}</p>
       <div className="add_to_cart">
-        <p>&dollar;${price}</p>
+        <p>&dollar;{props.price}</p>
         <button
           type="button"
           className="btn-add-to-cart"
-          data-name="${title}"
-          data-price="${price}"
-          onclick="addToCart(this)"
+          // data-name="${title}"
+          // data-price="${price}"
+          // onclick="addToCart(this)"
         >
           <Cart />
           Add to Cart
--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './css/ProductDisplay.css';
 // import productData from './ProductsData/products.json'
+import Product from "./Product";
 
 export default function ProductDisplay() {
   return (
     <>
       <section id="product_display">
         <h1>Our Products</h1>
-        
+        <Product imageUrl='./Images/hero-bcg.jpeg' title='Bed' price='200'/>
       </section>
     </>
   );
